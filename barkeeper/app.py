287e53320e88b8d768bfb5baa917a785e89ea916
Austin Marshall <oxtopus@gmail.com>
2012-09-03 17:46:28 -0700

    Lightweight Flask app to provide a view-only REST interface to Apache Zookeeper

---
+++ barkeeper/app.py
@@ -0,0 +1,45 @@
+from functools import partial
+import json
+from kazoo.client import KazooClient
+from flask import Flask, make_response, Response
+import zookeeper
+
+app = Flask(__name__)
+
+encode = partial(json.dumps, indent=2)
+error_response = lambda response, status, *args, **kwargs: \
+    make_response(encode({'reason': response, 'status': status}), status)
+
+def normalize_path(path):
+    return '/' + '/'.join(filter(None, path.split('/')))    
+
+@app.route('/favicon.ico')
+def favicon():
+    return ""
+    
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    node = normalize_path(path)
+    try:
+        value, znode = zk.get(node)
+
+        if znode.numChildren > 0:
+            children = zk.get_children(node)
+        else:
+            children = []
+
+        return Response(
+            encode([value, znode._asdict(), children]), \
+            content_type='application/json')
+
+    except zookeeper.NoNodeException:
+        return error_response('Not found', 404)
+    
+    except Exception as e:
+        return error_response(str(e), 500)
+
+if __name__ == "__main__":
+    zk = KazooClient()
+    zk.start()
+    app.run()
